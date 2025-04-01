1f699e1105494baac701ef91cac92832d14992a0
Djordje Cvijanovic <djordjec@mts-si.rs>
2025-04-01 13:14:10 +0200

    test

--- index.js
+++ index.js
@@ -8,13 +8,6 @@ import { titleRoute } from "./routes/titleRoutes.js";
 import errorHandler from "./middleware/errorMiddleware.js";
 import fileUpload from "express-fileupload";
 //test
-//test
-//123
-//45
-//56
-//78
-//910
-//12121
 dotenv.config();
 
 connectDB();
